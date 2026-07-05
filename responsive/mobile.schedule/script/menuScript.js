2ec98b87f882d40c7827b3a7dfa3353a45de2855
JustJoeee <valcked.camp@gmail.com>
2026-07-05 03:04:09 +0200

    d

--- responsive/mobile.schedule/script/menuScript.js
+++ responsive/mobile.schedule/script/menuScript.js
@@ -127,3 +127,31 @@ document.addEventListener("click",()=>{
     });
 
 });
+
+
+// Up Button
+
+document.addEventListener("DOMContentLoaded", () => {
+
+    const scrollBtn = document.getElementById("scrollTopBtn");
+
+    window.addEventListener("scroll", () => {
+
+        if (window.scrollY > 300) {
+            scrollBtn.classList.add("show");
+        } else {
+            scrollBtn.classList.remove("show");
+        }
+
+    });
+
+    scrollBtn.addEventListener("click", () => {
+
+        window.scrollTo({
+            top: 0,
+            behavior: "smooth"
+        });
+
+    });
+
+});
